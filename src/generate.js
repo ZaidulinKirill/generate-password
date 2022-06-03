e91cb4af8ec295f5b9c05852cff66e7164fae2cc
ZaidulinKirill <zaidulinkirill@gmail.com>
2022-06-03 20:43:16 +0300

    Update generate.js

--- src/generate.js
+++ src/generate.js
@@ -1,64 +1,39 @@
-var crypto = require('crypto');
-
-const RANDOM_BATCH_SIZE = 256;
-
-var randomIndex;
-var randomBytes;
-
-var getNextRandomValue = function() {
-	if (randomIndex === undefined || randomIndex >= randomBytes.length) {
-		randomIndex = 0;
-		randomBytes = crypto.randomBytes(RANDOM_BATCH_SIZE);
-	}
-
-	var result = randomBytes[randomIndex];
-	randomIndex += 1;
-
-	return result;
-};
-
-// Generates a random number
-var randomNumber = function(max) {
-	// gives a number between 0 (inclusive) and max (exclusive)
-	var rand = getNextRandomValue();
-	while (rand >= 256 - (256 % max)) {
-		rand = getNextRandomValue();
-	}
-	return rand % max;
-};
+function randomNumber(max) {
+	return Math.floor(Math.random() * max);
+}
 
 // Possible combinations
-var lowercase = 'abcdefghijklmnopqrstuvwxyz',
-	uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-	numbers = '0123456789',
-	symbols = '!@#$%^&*()+_-=}{[]|:;"/?.><,`~',
-	similarCharacters = /[1ilLI|`oO0]/g,
-	strictRules = [
-		{ name: 'lowercase', rule: /[a-z]/ },
-		{ name: 'uppercase', rule: /[A-Z]/ },
-		{ name: 'numbers', rule: /[0-9]/ },
-		{ name: 'symbols', rule: /[!@#$%^&*()+_\-=}{[\]|:;"/?.><,`~]/ }
-	];
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()+_-=}{[]|:;"/?.><,`~';
+const similarCharacters = /[1ilLI|`oO0]/g;
+const strictRules = [
+	{ name: 'lowercase', rule: /[a-z]/ },
+	{ name: 'uppercase', rule: /[A-Z]/ },
+	{ name: 'numbers', rule: /[0-9]/ },
+	{ name: 'symbols', rule: /[!@#$%^&*()+_\-=}{[\]|:;"/?.><,`~]/ },
+];
 
-var generate = function(options, pool) {
-	var password = '',
-		optionsLength = options.length,
-		poolLength = pool.length;
+var generate = function (options, pool) {
+	let password = '';
+	const optionsLength = options.length;
+	const poolLength = pool.length;
 
-	for (var i = 0; i < optionsLength; i++) {
+	for (let i = 0; i < optionsLength; i++) {
 		password += pool[randomNumber(poolLength)];
 	}
 
 	if (options.strict) {
 		// Iterate over each rule, checking to see if the password works.
-		var fitsRules = strictRules.every(function(rule) {
+		const fitsRules = strictRules.every((rule) => {
 			// If the option is not checked, ignore it.
 			if (options[rule.name] == false) return true;
 
 			// Treat symbol differently if explicit string is provided
 			if (rule.name === 'symbols' && typeof options[rule.name] === 'string') {
 				// Create a regular expression from the provided symbols
-				var re = new RegExp('['+options[rule.name]+']');
+				const re = new RegExp(`[${options[rule.name]}]`);
 				return re.test(password);
 			}
 
@@ -75,7 +50,7 @@ var generate = function(options, pool) {
 };
 
 // Generate a random password.
-module.exports.generate = function(options) {
+module.exports.generate = function (options) {
 	// Set defaults.
 	options = options || {};
 	if (!Object.prototype.hasOwnProperty.call(options, 'length')) options.length = 10;
@@ -88,14 +63,14 @@ module.exports.generate = function(options) {
 	if (!Object.prototype.hasOwnProperty.call(options, 'strict')) options.strict = false;
 
 	if (options.strict) {
-		var minStrictLength = 1 + (options.numbers ? 1 : 0) + (options.symbols ? 1 : 0) + (options.uppercase ? 1 : 0);
+		const minStrictLength = 1 + (options.numbers ? 1 : 0) + (options.symbols ? 1 : 0) + (options.uppercase ? 1 : 0);
 		if (minStrictLength > options.length) {
 			throw new TypeError('Length must correlate with strict guidelines');
 		}
 	}
 
 	// Generate character pool
-	var pool = '';
+	let pool = '';
 
 	// lowercase
 	if (options.lowercase) {
@@ -130,21 +105,21 @@ module.exports.generate = function(options) {
 	}
 
 	// excludes characters from the pool
-	var i = options.exclude.length;
+	let i = options.exclude.length;
 	while (i--) {
 		pool = pool.replace(options.exclude[i], '');
 	}
 
-	var password = generate(options, pool);
+	const password = generate(options, pool);
 
 	return password;
 };
 
 // Generates multiple passwords at once with the same options.
-module.exports.generateMultiple = function(amount, options) {
-	var passwords = [];
+module.exports.generateMultiple = function (amount, options) {
+	const passwords = [];
 
-	for (var i = 0; i < amount; i++) {
+	for (let i = 0; i < amount; i++) {
 		passwords[i] = module.exports.generate(options);
 	}
 
